7f5fcb12ec2d3aa89c1430117fc650612c28a8e9
Abhinav Saxena <36484806+Abhinavsaxenaa@users.noreply.github.com>
2018-02-16 23:52:18 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/aeticle-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'aeticle-one.html'));
 });
+
 app.get('/article-two',function(req,res){
     res.send('Article two is requested and will be served here');
 });
+
 app.get('/article-three',function(req,res){
     res.send('Article three is requested and will be served here');
 });
