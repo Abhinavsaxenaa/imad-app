a0da5201d1f80f803b425ae7fc97eb76ccd66cda
Abhinav Saxena <36484806+Abhinavsaxenaa@users.noreply.github.com>
2018-02-17 09:28:57 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlesName',function(req,res){
-    var articleName=req.params.articleName;
+    var articlesName=req.params.articleName;
   res.send(createtemplate(articles[articlesName]));
 });
 
