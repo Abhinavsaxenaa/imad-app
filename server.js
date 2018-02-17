0fa5c30fd159972dfcd5f10e2112cc0a71ea5a8b
Abhinav Saxena <36484806+Abhinavsaxenaa@users.noreply.github.com>
2018-02-17 00:09:13 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aeticle-one',function(req,res){
+app.get('/1',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'aeticle-one.html'));
 });
 
