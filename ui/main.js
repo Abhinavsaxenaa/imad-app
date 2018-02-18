f8b16ad615ed6d6461a9b9ca5f0959bdc0979c48
Abhinav Saxena <36484806+Abhinavsaxenaa@users.noreply.github.com>
2018-02-17 23:09:41 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element = document.getElementById("text1");
 element.innerHTML="bye bye";
 
 var element1 = document.getElementById("img1");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    element1.style.marginLeft=marginLeft+"px";
+}
 element1.onclick=function() {
-    element1.style.marginLeft='100px';
+    var time = setInterval(moveRight,100);
 };
