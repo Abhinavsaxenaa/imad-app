b7b6783aec56b1e67ab0ed55ece746d58081d06e
Abhinav Saxena <36484806+Abhinavsaxenaa@users.noreply.github.com>
2018-02-17 22:40:00 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById("text1");
 element.innerHTML="bye bye";
+
+var element1 = document.getElementByID("img1");
+element1.onclick=function() {
+    element1.style.marginleft='10px';
+};
